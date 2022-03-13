77a8b32633c9201f685361d79a1ece2fed78a02c
Thayna Falkemback Vicari <94264518+thaythevenon@users.noreply.github.com>
2022-03-12 23:02:37 -0300

    Add files via upload

---
+++ dec.py
@@ -0,0 +1,8 @@
+def dec2bin(n):
+    return(n)
+n = 21
+    
+def __index__(self):
+        return self.n
+        
+print('O numero binario é :',bin(n))
---
+++ ve.py
@@ -0,0 +1,10 @@
+class ve:
+    express = (2 * 2 - 4)
+    
+    def __index__(self):
+        return self.express
+    if express == True:
+          print("valido")
+    else:
+        print("invalido")
+        
